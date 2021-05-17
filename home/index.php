c7d767ec20bee5a58c0a6a0a8826942ce632322a
Kiattiyot Sihawong <kaittiyost.oke@gmail.com>
2021-05-17 18:42:33 +0700

    commit

--- home/index.php
+++ home/index.php
@@ -12,10 +12,10 @@
 		<div class="alert alert-info text-dark" style="width:100%">
 			<div class="alert-body">
 				<h2>รายการทั้งหมด</h2>
-			<button type="button" class="btn btn-success" data-toggle="modal" data-target="#add_debtor_modal"><i class="fa fa-plus"></i> เพิ่มผู้ติดหนี้</button>
+				 <input type="search" class="form-control" placeholder="ค้นหาเลขเด็ด" onKeyup="findDebtor(this.value)">
 			</div>
 		</div>
-	   	   <input type="search" class="form-control" placeholder="ค้นหา" onKeyup="findDebtor(this.value)">
+	   	  
 		<br>
 		<center>
 		<div id="deb_component">
@@ -23,20 +23,23 @@
 					<div class="col col-6">
 									       <div class="card border-info" style="text-align:center;">
 									       		<div class="card-header bg-white" style="height:40px;">
-									       		   <p  style="font-size:14px" class="badge badge-light">ทดสอบ</p><br>
+									       		   <p  style="font-size:14px" class="badge badge-light">779 998</p><br>
 									       		</div>
 												<div class="card-body" style="overflow:auto;">
-													<img style="width:80px;height:80px" class="img-thumbnail" src="../images/lot/test.jpg"><br><br>
+													<img  class="responsive_img" src="../images/item/lotto_excemple.jpeg"><br><br>
+													<button type="button" class="btn btn-info" style="width: 100%"><i class="fas fa-search-plus"></i></button>
 												</div>
 										  </div>
 						</div>
 						<div class="col col-6">
 									       <div class="card border-info" style="text-align:center;">
 									       		<div class="card-header bg-white" style="height:40px;">
-									       		   <p  style="font-size:14px" class="badge badge-light">ทดสอบ</p><br>
+									       		   <p  style="font-size:14px" class="badge badge-light">666 099</p><br>
 									       		</div>
 												<div class="card-body" style="overflow:auto;">
-													<img style="width:80px;height:80px" class="img-thumbnail" src="../images/lot/test.jpg"><br><br>
+													<img  class="responsive_img" src="../images/item/lotto_excemple.jpeg"><br><br>
+													<button type="button" class="btn btn-info" style="width: 100%"><i class="fas fa-search-plus"></i></button>
+
 												</div>
 										  </div>
 						</div>
@@ -46,36 +49,6 @@
 	</div>
 	</div>
 
-<!-- Modal -->
-<div class="modal fade" id="add_debtor_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header bg-primary text-light">
-        <h5 class="modal-title">เพิ่มผู้ติดหนี้รายใหม่</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      	  <label for="img_profile">รูปโปรไฟล์</label><br>
-      	<input type="file" id="img_profile"><br>
-      	 <label for='name'>ชื่อ-นามสกุล</label>
-        <input type="text" id="name" class="form-control" placeholder="ขื่อ-นามสกุล">
-         <label for='total'>ยอดเงิน</label>
-        <input type="number" id="total" class="form-control" placeholder="ยอดเงิน(บาท)">
-          <label for='tel'>เบอร์ติดต่อ</label>
-        <input type="text" id="tel" class="form-control" placeholder="เบอร์">
-          <label for='addr'>ที่อยู่</label>
-        <input type="text" id="addr" class="form-control" placeholder="ที่อยู่">
-          <label for='deb_desc'>คำอธิบาย</label>
-        <textarea id="deb_desc" style="height:200px" class="form-control" placeholder="ช้อมูลต่างๆ"></textarea>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-outline-primary" onClick="addDebtor()">ยืนยัน</button>
-      </div>
-    </div>
-  </div>
-</div>
 <script src='../resource/script.js'></script>
 </body>
 </html>
--- resource/include/menu.php
+++ resource/include/menu.php
@@ -6,9 +6,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="syleshoeet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Kanit|Merriweather" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <!-- font awesome-->
+    <script src="https://kit.fontawesome.com/989ec22c17.js" crossorigin="anonymous"></script>
+
+
 <style>
+  *,h4,h5{
+     font-family: 'Kanit', sans-serif;
+  }
+
   body{
-    background-color:#F3F1F1;
+    background-color:white;
     background-position: center center;
     background-repeat: no-repeat;
     background-attachment: fixed;
@@ -17,31 +30,35 @@
    #loading_button{
       display:none;
    }
+
+  .responsive_img {
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+  }
 </style>
-<div style="background-color:#E3E3E3;height:50px;position:fixed;width:100%;z-index:1000;font-size:20px;opacity:0.9;">
-  <table class="table"> 
-  <tr>
-    <td style="text-align:center;">
-      <a href="#" class="text-dark">
-          <i   class="fa fa-history"></i>
-      </a>
-    </td>
-    <td style="text-align:center;">
-    <a href="#">
-          <i class="fa fa-home"></i>
-      </a>
-    </td>
-    <td style="text-align:center;">
-      <a href="#" class="text-success">
-          <i class="fa fa-check-circle"></i>
-      </a>
-    </td>
-    <td style="text-align:center;">
-      <a href="/huay/index.php" class="text-danger">
-          <i class="fa fa-sign-out"></i>
-      </a>
-    </td>
-  </tr>
-  </table>
+<div style="background-color:#E3E3E3;height:50px;position:fixed;width:100%;z-index:1000;font-size:20px;opacity:1;">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Lottoo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fas fa-home"></i> หน้าแรก <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-user"></i> โปรไฟล์</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      
+    </span>
+  </div>
+</nav>
 </div>
 
