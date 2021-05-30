1ccfe7ce53326ad1175b83815ddc46457d39d4e9
dragon111333 <dragon123789555@gmail.com>
2021-05-30 20:04:03 +0700

    555

--- resource/include/menu.php
+++ resource/include/menu.php
@@ -1,13 +1,13 @@
 <div class="menu">
-  <div class="title" onclick="openDropDownMenu()">แสงทองขายเลข<span class="fa fa-bars"></span>
+  <div class="title" onclick="openDropDownMenu()">แสงทองขายเลข<span class="menu-span fa fa-bars"></span>
     <div class="arrow"></div>
   </div>
   <div class="dropdown">
-    <a href="/home/"><p>ร้านค้าลอตเตอรี่ <span class="fas fa-store"></span></p></a>
-    <a  href="/cart/"><p>ตระกร้าสินค้า <span class="fas fa-shopping-basket"></span> <span class="badge badge-pill badge-danger" id="cart_count">0</span></p></a>
-    <a  href="/purchase_list/"><p>การสั่งซื้อ <span class="fas fa-bars"></span></p></a>
-    <a  href="/profile/"><p>โปรไฟล์ <span class="fas fa-user"></span></p></a>
-    <p id="logout_btn">ออกจากระบบ <span class="fa fa-sign-out"></span></p>
+    <a href="/home/"><p>ร้านค้าลอตเตอรี่ <span class="menu-span fas fa-store"></span></p></a>
+    <a  href="/cart/"><p>ตระกร้าสินค้า <span class="menu-span fas fa-shopping-basket"></span> <span class="badge badge-pill badge-danger" id="cart_count">0</span></p></a>
+    <a  href="/purchase_list/"><p>การสั่งซื้อ <span class="menu-span fas fa-bars"></span></p></a>
+    <a  href="/profile/"><p>โปรไฟล์ <span class="menu-span fas fa-user"></span></p></a>
+    <p id="logout_btn">ออกจากระบบ <span class="menu-span fa fa-sign-out"></span></p>
   </div>
 </div>
 <br>
@@ -15,7 +15,7 @@
 <style>
     .menu{width:80%;cursor:pointer;z-index:3000;position:fixed;left:10%;}
     .title{width:100%;box-sizing:border-box;background:#fff;padding:14px;border-radius:4px;position:relative;box-shadow:0 0 40px -10px #000;color:#505050}
-    span{float:right;font-size:18px!important}
+    .menu-span{float:right;font-size:18px!important}
     .dropdown{width:100%;background:#fff;border-radius:4px;box-shadow:0 0 40px -10px #000;color:#505050;margin-top:24px;max-height:0;h;overflow:hidden;transition:all .5s}
     .down{max-height:250px}
     .arrow{border-left:10px solid transparent;border-right:10px solid transparent;border-bottom:10px solid #fff;position:absolute;right:20px;bottom:-24px;display:none}
