b9a735cf38840671a82b3bb9b9bf655b65d4d5e6
dragon111333 <dragon123789555@gmail.com>
2021-06-07 00:29:34 +0700

    google login 100%

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
 	<?php include(__DIR__."/resource/include/script.html"); ?>
 </head>
 <body>
+	<meta name="google-signin-client_id" content="1036552871785-hp22bf6mmcfgdto7nrkajn39s558on4j.apps.googleusercontent.com">
 	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v10.0&appId=527242314963433&autoLogAppEvents=1" nonce="khs2hvpO"></script> 	<br><br><br>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v10.0&appId=527242314963433&autoLogAppEvents=1" nonce="PywkciAJ"></script>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
 	<center>
 		<div class="container">
 			<br><br>
@@ -16,32 +18,12 @@
 						<div class="card-body">
 							<h1>Sangtian lotto</h1>
 							<p style="font-size:10px">เว็บแอพลิเคชั่นขายลอตเตอรี่ออนไลน์</p>
-							<p class="badge badge-pill badge-warning text-light" >กรุณาเข้าสู่ระบบด้วยfacebook</p>
+							<p class="badge badge-pill badge-warning text-light" >กรุณาเข้าสู่ระบบ</p>
 							<br>
-							<!-- 
-							<form style="width:80%">
-								<div class="input-group mb-2">
-									<div class="input-group-prepend">
-										<div class="input-group-text">
-											<i class="fas fa-user"></i>
-										</div>
-									</div>
-									<input type="text" class="form-control" id="username" placeholder="username">
-								</div>
-								<div class="input-group mb-2">
-									<div class="input-group-prepend">
-										<div class="input-group-text">
-											<i class="fas fa-unlock"></i>
-										</div>
-									</div>
-									<input type="password" class="form-control" id="password" placeholder="รหัสผ่าน">
-								</div><a href="/forgot_password/" style="text-align:right;margin-left: 60%;font-size: :10px;" >ลืมรหัสผ่าน?</a><br><br>
-								<button type="button" onClick="login()" class="btn btn-outline-primary" style="width:60%;"><i class="fas fa-sign-in-alt"></i> เข้าสู่ระบบ</button>
-								<br><br> -->
-								<div onlogin="checkLoginStateFB();" class="fb-login-button" data-width="" data-size="medium" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>
+								<div onlogin="checkLoginStateFB();" class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div>
 								<br><br>
-								<!-- <button type="button" onClick="go_register()" class="btn btn-outline-warning"><i class="fas fa-user-plus"></i> สมัครสมาชิก</button>
-							</form> -->
+								<div data-onsuccess="googleLogin" class="g-signin2" data-width="250"></div>
+								<br><br>				
 						</div>
 						<div>
 						</div>
@@ -71,7 +53,6 @@
 					});
 				}
 				function loginByFB(data){
-					console.log(data)
 					$.ajax({
 						"type":"POST",
 						"contentType":"application/x-www-form-urlencoded;charset=utf-8",
@@ -79,6 +60,25 @@
 						"data":{"fb_id":data.id,"fb_name":data.name,"func":"loginFb"}
 					})
 					.done((response)=>{
+						if(String(response)==="new_user"||String(response)==="old_user"){
+							location.href = projectPath+'/home';
+						}
+					});
+				}
+
+				function googleLogin(googleUser) {
+					var profile = googleUser.getBasicProfile();
+					let userData ={};
+					userData.tokenId = googleUser.getAuthResponse().id_token;
+					userData.func = "loginGoogle";
+					$.ajax({
+						"type":"POST",
+						"contentType":"application/x-www-form-urlencoded;charset=utf-8",
+						"url":projectPath+"/resource/controller/login.php",
+						"data":userData
+					})
+					.done((response)=>{
+						console.log(response);
 						if(String(response)==="new_user"||String(response)==="old_user"){
 							location.href = projectPath+'/home';
 						}
--- resource/controller/login.php
+++ resource/controller/login.php
@@ -2,6 +2,7 @@
     session_start();
     include(__DIR__."/../database/db_config.php");
     include(__DIR__."/../include/util.php");
+    require(__DIR__."/../ex_package/google/autoload.php");
     // function login(){
     //     $conn = DB::getConnect();
 
@@ -24,7 +25,8 @@
             $conn = DB::getConnect();
             $fbId = htmlentities($conn->escape_string($userData["fb_id"]));
             $fbName = htmlentities($conn->escape_string($userData["fb_name"]));
-            $sql = "SELECT * FROM fb_user WHERE FB_ID=".$fbId;
+            $sql = "SELECT user.* FROM fb_user INNER JOIN user ON fb_user.user_id = user.USER_ID \n".
+                    " WHERE FB_ID=".$fbId;
             $result = $conn->query($sql);
             if(($conn->affected_rows)>0){
                 $userData = $result->fetch_array();
@@ -49,6 +51,45 @@
             echo "Error -->".$th->getMessage();
         }
     }
+
+    function loginGoogle($userData){
+        try {
+            $CLIENT_ID = "1036552871785-hp22bf6mmcfgdto7nrkajn39s558on4j.apps.googleusercontent.com";
+            $client = new Google_Client(['client_id' => $CLIENT_ID]); 
+            $payload = $client->verifyIdToken($userData["tokenId"]);
+            if ($payload){
+                $userid = $payload['sub'];
+                $conn = DB::getConnect();
+                $sql = "SELECT user.* FROM gg_user INNER JOIN userON gg_user.user_id = user.USER_ID\n".
+                        "WHERE gg_user.user_id =".$userid;
+                $result = $conn->query($sql);
+                if(($conn->affected_rows)>0){
+                    $userData = $result->fetch_array();
+                    $_SESSION['userData'] = $userData;
+                    $_SESSION['loginStatus'] = true;
+                    echo "old_user";
+                }else{
+                    $fullName = explode(" ",$payload["name"]);
+                    $sql = "INSERT INTO user SET USER_USERNAME='".$userid."',USER_PASSWORD='".hash("sha256",Util::randomPassword(),false)."'".
+                            ",USER_NAME='".$fullName[0]."',USER_LASTNAME='".(string)$fullName[(sizeof($fullName)-1)]."',USER_EMAIL='".$payload["email"]."'";
+                    if($conn->query($sql)){
+                        $lastId = $conn->insert_id;
+                        $conn->query("INSERT INTO gg_user SET user_id=".$lastId.",gg_id=".$userid);
+                        $result = $conn->query("SELECT * FROM user WHERE USER_ID=".$lastId." LIMIT 1");
+                        $userData = $result->fetch_array();
+                        $_SESSION['userData'] = $userData;
+                        $_SESSION['loginStatus'] = true;
+                        echo "new_user";
+                    }
+                }
+            } else {
+                echo "0";
+            }
+        } catch (Exception $th) {
+            echo "Error -->".$th->getMessage();
+        }
+    }
+
     function logout(){
         try {
             session_destroy();
@@ -65,6 +106,9 @@
             //     break;
             case "loginFb":
                 loginFb($_POST);
+                break;
+            case "loginGoogle":
+                loginGoogle($_POST);
                 break;
             case "logout" :
                 logout();
